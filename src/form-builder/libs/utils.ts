df31d4ac69971e11eba429aba5131a8cb74e9a78
Subu19 <subuacharya19@gmail.com>
2025-03-04 18:19:00 +0545

    add remove element by  field type function

--- src/form-builder/libs/utils.ts
+++ src/form-builder/libs/utils.ts
@@ -36,3 +36,33 @@ export const disableAllElements = (
     }
   });
 };
+export const removeElementsByFieldType = (
+  formElements: FormElementOrList[],
+  fieldTypeToRemove: string
+): FormElementOrList[] => {
+  const filterElement = (formElement: FormElement): boolean => {
+    return formElement.fieldType !== fieldTypeToRemove;
+  };
+
+  return formElements
+    .map((element) => {
+      if (Array.isArray(element)) {
+        // If the element is an array, recursively process each item in the array
+        return element.filter(filterElement).map((subElement) => subElement);
+      } else if ((element as unknown as FormStep).stepFields !== undefined) {
+        // If the element is a FormStep, recursively process its stepFields
+        const formStep = element as unknown as FormStep;
+        return {
+          ...formStep,
+          stepFields: removeElementsByFieldType(
+            formStep.stepFields,
+            fieldTypeToRemove
+          ),
+        } as unknown as FormElement[];
+      } else {
+        // If the element is a FormElement, check and filter it
+        return filterElement(element as FormElement) ? element : null;
+      }
+    })
+    .filter(Boolean) as FormElementOrList[];
+};
